c4605b7ccda3cfbc8d0e4aec9253dee574f920a3
Brian Smith <bksmith1998@gmail.com>
2021-11-30 23:28:13 -0500

    Settings Activity Updated

--- app/src/main/java/com/example/rememberme/MainActivity.kt
+++ app/src/main/java/com/example/rememberme/MainActivity.kt
@@ -5,17 +5,25 @@ import android.os.Bundle
 import android.content.Intent
 import android.view.View
 import android.widget.Button
+import androidx.preference.PreferenceManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        PreferenceManager.setDefaultValues(this, R.xml.root_preferences, false)
 
+        val mButton = findViewById<View>(R.id.Nextbutton) as Button
+        mButton.setOnClickListener {
+            startActivity((Intent(this@MainActivity, EventPage::class.java)))
 
-    val mButton = findViewById<View>(R.id.Nextbutton) as Button
-    mButton.setOnClickListener {
-        startActivity((Intent(this@MainActivity, EventPage::class.java)))
+    }
+        val SButton = findViewById<View>(R.id.SettingsBT) as Button
+        SButton.setOnClickListener {
+            startActivity((Intent(this@MainActivity, SettingsActivity::class.java)))
+        }
+
     }
 
     }
-}
+
