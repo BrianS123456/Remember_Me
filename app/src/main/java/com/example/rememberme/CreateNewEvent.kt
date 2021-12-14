cb163f015df444352a91b82a545fc74016d69658
Brian Smith <bksmith1998@gmail.com>
2021-12-14 13:43:19 -0500

    Adding User Input to CreateNewEvent.kt for Title, Date, and Time Event Page Updated

--- app/src/main/java/com/example/rememberme/CreateNewEvent.kt
+++ app/src/main/java/com/example/rememberme/CreateNewEvent.kt
@@ -3,30 +3,27 @@ package com.example.rememberme
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import java.sql.Time
 
 class CreateNewEvent : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_new_event)
-        //val timeText = findViewById<EditText>(R.id.editTextTime)
-        //val dateText = findViewById<EditText>(R.id.editTextDate)
-        //val titleText = findViewById<EditText>(R.id.editTextTitle)
-        val mButton = findViewById(R.id.Newbt) as Button
+        val timeText = findViewById<EditText>(R.id.timeT)
+        val dateText = findViewById<EditText>(R.id.dateD)
+        val titleText = findViewById<EditText>(R.id.eventE)
+        val mButton = findViewById<Button>(R.id.CreateNewBT)
         mButton.setOnClickListener {
-         //   val time = timeText.text.toString()
-           // val date = dateText.text.toString()
-            //val title = titleText.text.toString()
+            val time = timeText.text.toString()
+            val date = dateText.text.toString()
+            val title = titleText.text.toString()
 
-            val intent = Intent(this@CreateNewEvent, EventPage::class.java)
-           // intent.putExtra("Title", title)
-           // intent.putExtra("Date", date)
-           // intent.putExtra("Time", time)
+            val intent = Intent(this, EventPage::class.java)
+            intent.putExtra("Title", title)
+            intent.putExtra("Date", date)
+            intent.putExtra("Time", time)
             startActivity(intent)
 
         }
--- app/src/main/java/com/example/rememberme/EventPage.kt
+++ app/src/main/java/com/example/rememberme/EventPage.kt
@@ -1,33 +1,31 @@
 package com.example.rememberme
 
 import android.content.Intent
-import android.database.Cursor
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class EventPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_event_page)
-        //val intent = intent
-        //val title = intent.getStringArrayExtra("Title")
-        //val date = intent.getStringArrayExtra("Date")
-        //val time = intent.getStringArrayExtra("Time")
 
-        //val textV = findViewById<TextView>(R.id.txtV)
-        //textV.text = "Title- "+title+"\nDate- "+date+"\nTime- " +time
-        val mButton = findViewById<View>(R.id.CreateNew) as Button
+        val mButton = findViewById<Button>(R.id.newBT)
         mButton.setOnClickListener {
 
 
-                startActivity((Intent(this@EventPage, CreateNewEvent::class.java)))
-            }
+            startActivity((Intent(this@EventPage, CreateNewEvent::class.java)))
+        }
+
+        val intent = getIntent()
+        val title = intent.getStringExtra("Title")
+        val date = intent.getStringExtra("Date")
+        val time = intent.getStringExtra("Time")
+
+        val textV = findViewById<TextView>(R.id.txtV)
+        textV.text = "Title: "+title+"\nDate: "+date+"\nTime: " +time
 
         }
 
